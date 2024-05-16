5998b26f6c39403bb56e4bb529e8cea5ec21450f
musamM921 <musaartlove@gmail.com>
2024-05-16 13:49:28 +0400

    initial commit

--- script.js
+++ script.js
@@ -14,46 +14,46 @@ class RandomUser {
         let cardElem = document.querySelector('.card');
         cardElem.innerHTML = `
         <div class = "card-head">
-                <a href = "#"><i class = "fas fa-envelope"></i> Email</a>
-                <div class = "user-image">
-                    <img src = "images/profile-img.jpg" alt = "">
-                </div>
+        <a href = "mailto:${user.email}"><i class = "fas fa-envelope"></i> Email</a>
+        <div class = "user-image">
+            <img src = "${user.picture.large}" alt = "">
+        </div>
+    </div>
+
+    <div class = "card-body">
+        <div class = "user-post-address">
+            <div>
+                <span>${user.location.street.number}</span><span>Street Address</span>
             </div>
-
-            <div class = "card-body">
-                <div class = "user-post-address">
-                    <div>
-                        <span>9545</span><span>Street Address</span>
-                    </div>
-                    <div>
-                        <span>20054</span><span>Postcode</span>
-                    </div>
-                    <div>
-                        <span>Esplanadi</span><span>Street Name</span>
-                    </div>
-                </div>
-
-                <div class = "user-name">
-                    <span class = "user-name-title">Mr.</span>
-                    <span class = "user-name-full">Helen Anttila,</span>
-                    <span class = "user-age">34</span>
-                </div>
-
-                <div class = "user-location-address">
-                    <span>Kouvola, Satakunta, Finland</span>
-                </div>
+            <div>
+                <span>${user.location.postcode}</span><span>Postcode</span>
             </div>
-
-            <div class = "card-foot">
-                <div class = "user-contact-info">
-                    <span>
-                        <i class = "fas fa-phone"></i> 07-567-990
-                    </span>
-                    <span>
-                        <i class = "fa-solid fa-mobile-button"></i> 044-391-36-63
-                    </span>
-                </div>
+            <div>
+                <span>${user.location.street.name}</span><span>Street Name</span>
             </div>
+        </div>
+
+        <div class = "user-name">
+            <span class = "user-name-title">${user.name.title}.</span>
+            <span class = "user-name-full">${user.name.first} ${user.name.last},</span>
+            <span class = "user-age">${user.dob.age}</span>
+        </div>
+
+        <div class = "user-location-address">
+            <span>${user.location.city}, ${user.location.state}, ${user.location.country}</span>
+        </div>
+    </div>
+
+    <div class = "card-foot">
+        <div class = "user-contact-info">
+            <span>
+                <i class = "fas fa-phone"></i> ${user.phone}
+            </span>
+            <span>
+                <i class = "fa-solid fa-mobile-button"></i> ${user.cell}
+            </span>
+        </div>
+    </div>
         `
     }
 }
